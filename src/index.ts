6849823bd5f6edad526247bb71a0ca839f8b720f
okonomiyakki <parkji12532@naver.com>
2024-02-11 11:22:10 +0900

    fix: 방아쇠 수정
    시작하는 기능만 하도록

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import EnvConfig from './configs/EnvConfig';
 import Runner from './works/Runner';
 
 /**
  * 작업을 시작합니다.
  */
 const main = async () => {
-  EnvConfig.parseEnv();
-
-  await new Runner(EnvConfig).run();
+  await new Runner().run();
 };
 
 main();
