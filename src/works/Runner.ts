d595cf8e74c25ff0052408b79ddbe829266ff0e7
okonomiyakki <parkji12532@naver.com>
2024-02-11 11:21:24 +0900

    fix: Runner는 이럭식으로 수정

--- src/works/Runner.ts
+++ src/works/Runner.ts
@@ -1,11 +1,18 @@
 import EnvConfig from '../configs/EnvConfig';
+import Approacher from './Approacher';
+import PageHandler from './PageHandler';
 
 /**
  * 정의해놓은 work들을 동작시킵니다.
  */
 export default class Runner {
-  constructor(private readonly confing: EnvConfig) {}
   async run() {
-    console.log(this.confing);
+    EnvConfig.initConfig();
+
+    const pageHandler = new PageHandler();
+
+    const approacher = new Approacher(EnvConfig.current, pageHandler);
+
+    approacher.accessPage();
   }
 }
