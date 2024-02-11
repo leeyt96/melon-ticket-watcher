450fa13f4f973bf63e8b7e8512af4aa5a2f9017f
okonomiyakki <parkji12532@naver.com>
2024-02-11 11:16:47 +0900

    fix: 환경변수 초기화 방법 수정함

--- src/configs/EnvConfig.ts
+++ src/configs/EnvConfig.ts
@@ -1,42 +1,45 @@
 import * as env from 'dotenv';
 
 export default class EnvConfig {
-  static url: { readonly MELON_TICKET_URL: string };
+  static current: EnvConfig;
 
-  static info: { readonly ID: string; readonly PW: string };
+  readonly URL: string;
 
-  static path: { readonly TOP_LOGIN_BUTTON: string };
+  readonly ID: string;
+  readonly PW: string;
 
-  private static load() {
+  readonly TOP_LOGIN_BUTTON: string;
+  readonly KAKAO_LOGIN_BUTTON: string;
+
+  /** current에다가 환경변수 초기화 */
+  static initConfig() {
+    this.current = EnvConfig.parseEnv();
+  }
+
+  /**
+   * EnvConfig 인스턴스 초기화 함수임
+   *
+   * @param env 환경변수
+   */
+  private static createConfig(env: EnvConfig) {
+    return Object.assign(new EnvConfig(), env);
+  }
+
+  /** env 로드하기 */
+  private static loadEnv() {
     env.config();
   }
 
-  static parseEnv() {
-    this.url = EnvConfig.getUrl();
-    this.info = EnvConfig.getInfo();
-    this.path = EnvConfig.getPath();
-  }
+  /** env 값들을 읽어와서 EnvConfig 인스턴스 생성 */
+  private static parseEnv() {
+    EnvConfig.loadEnv();
 
-  private static getUrl() {
-    EnvConfig.load();
-
-    return {
-      MELON_TICKET_URL: process.env.MELON_TICKET_URL,
-    };
-  }
-  private static getInfo() {
-    EnvConfig.load();
-
-    return {
+    return this.createConfig({
+      URL: process.env.URL,
       ID: process.env.ID,
       PW: process.env.PW,
-    };
-  }
-  private static getPath() {
-    EnvConfig.load();
-
-    return {
       TOP_LOGIN_BUTTON: process.env.TOP_LOGIN_BUTTON,
-    };
+      KAKAO_LOGIN_BUTTON: process.env.KAKAO_LOGIN_BUTTON,
+    });
   }
 }
