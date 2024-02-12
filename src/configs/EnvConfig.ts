1fc13e4b806784bcd7531afb2c0596d5911736f7
okonomiyakki <parkji12532@naver.com>
2024-02-12 11:59:40 +0900

    feat: 희망하는 회차 및 좌석 등급을 탐색하기위한 경로변수 load하기
    - defaultCondition에다가 따로 빼서 Indicator로 넘겨주자
    - url, login, path도 각각 나눠서 로드하지만, 초기화는 한꺼번에 하도록 함

--- src/configs/EnvConfig.ts
+++ src/configs/EnvConfig.ts
@@ -1,27 +1,75 @@
 import * as env from 'dotenv';
 
 export default class EnvConfig {
-  static current: EnvConfig;
+  static defaultCondition: Partial<EnvConfig>;
+  static url: Partial<EnvConfig>;
+  static kakaoLogin: Partial<EnvConfig>;
+  static path: Partial<EnvConfig>;
 
-  readonly URL: string;
+  readonly DATE_3_2: string;
+  readonly DATE_3_3: string;
+  readonly DATE_3_9: string;
+  readonly DATE_3_10: string;
+
+  readonly TARGET_DATE_3_2: string;
+  readonly TARGET_DATE_3_3: string;
+  readonly TARGET_DATE_3_9: string;
+  readonly TARGET_DATE_3_10: string;
+
+  readonly SECTION_R: string;
+  readonly SECTION_S: string;
+  readonly SECTION_A: string;
+
+  readonly TARGET_SECTION_DATE_R_3_2: string;
+  readonly TARGET_SECTION_DATE_S_3_2: string;
+  readonly TARGET_SECTION_DATE_A_3_2: string;
+
+  readonly TARGET_SECTION_DATE_R_3_3: string;
+  readonly TARGET_SECTION_DATE_S_3_3: string;
+  readonly TARGET_SECTION_DATE_A_3_3: string;
+
+  readonly TARGET_SECTION_DATE_R_3_9: string;
+  readonly TARGET_SECTION_DATE_S_3_9: string;
+  readonly TARGET_SECTION_DATE_A_3_9: string;
+
+  readonly TARGET_SECTION_DATE_R_3_10: string;
+  readonly TARGET_SECTION_DATE_S_3_10: string;
+  readonly TARGET_SECTION_DATE_A_3_10: string;
+
+  readonly TARGET_AREA_R: string;
+  readonly TARGET_AREA_S: string;
+  readonly TARGET_AREA_A: string;
+
+  readonly MELON_TICKET: string;
 
   readonly ID: string;
   readonly PW: string;
 
   readonly TOP_LOGIN_BUTTON: string;
   readonly KAKAO_LOGIN_BUTTON: string;
+  readonly KAKAO_LOGIN_ID_INPUT: string;
+  readonly KAKAO_LOGIN_PW_INPUT: string;
+  readonly KAKAO_LOGIN_SUBMIT_BUTTON: string;
 
-  /** current에다가 환경변수 초기화 */
-  static initConfig() {
-    this.current = EnvConfig.parseEnv();
+  /** 탐색을 희망하는 모든 경우를 defaultCondition으로 초기화 */
+  static initDefaultConditionConfig() {
+    this.defaultCondition = EnvConfig.parseDefaultConditionEnv();
+  }
+
+  /** 각 역할별로 초기화 */
+  static initProcessConfig() {
+    this.url = EnvConfig.parseUrlEnv();
+    this.kakaoLogin = EnvConfig.parseKakaoLoginEnv();
+    this.path = EnvConfig.parsePathEnv();
   }
 
   /**
    * EnvConfig 인스턴스 초기화 함수임
    *
    * @param env 환경변수
+   * @returns EnvConfig 인스턴스
    */
-  private static createConfig(env: EnvConfig) {
+  private static createConfig(env: Partial<EnvConfig>) {
     return Object.assign(new EnvConfig(), env);
   }
 
@@ -30,16 +78,75 @@ export default class EnvConfig {
     env.config();
   }
 
-  /** env 값들을 읽어와서 EnvConfig 인스턴스 생성 */
-  private static parseEnv() {
+  /** 탐색을 희망하는 모든 경우를 담은 EnvConfig 인스턴스 만들기 */
+  private static parseDefaultConditionEnv() {
     EnvConfig.loadEnv();
 
     return this.createConfig({
-      URL: process.env.URL,
+      DATE_3_2: process.env.DATE_3_2,
+      DATE_3_3: process.env.DATE_3_3,
+      DATE_3_9: process.env.DATE_3_9,
+      DATE_3_10: process.env.DATE_3_10,
+
+      TARGET_DATE_3_2: process.env.TARGET_DATE_3_2,
+      TARGET_DATE_3_3: process.env.TARGET_DATE_3_3,
+      TARGET_DATE_3_9: process.env.TARGET_DATE_3_9,
+      TARGET_DATE_3_10: process.env.TARGET_DATE_3_10,
+
+      SECTION_R: process.env.SECTION_R,
+      SECTION_S: process.env.SECTION_S,
+      SECTION_A: process.env.SECTION_A,
+
+      TARGET_SECTION_DATE_R_3_2: process.env.TARGET_SECTION_DATE_R_3_2,
+      TARGET_SECTION_DATE_S_3_2: process.env.TARGET_SECTION_DATE_S_3_2,
+      TARGET_SECTION_DATE_A_3_2: process.env.TARGET_SECTION_DATE_A_3_2,
+
+      TARGET_SECTION_DATE_R_3_3: process.env.TARGET_SECTION_DATE_R_3_3,
+      TARGET_SECTION_DATE_S_3_3: process.env.TARGET_SECTION_DATE_S_3_3,
+      TARGET_SECTION_DATE_A_3_3: process.env.TARGET_SECTION_DATE_A_3_3,
+
+      TARGET_SECTION_DATE_R_3_9: process.env.TARGET_SECTION_DATE_R_3_9,
+      TARGET_SECTION_DATE_S_3_9: process.env.TARGET_SECTION_DATE_S_3_9,
+      TARGET_SECTION_DATE_A_3_9: process.env.TARGET_SECTION_DATE_A_3_9,
+
+      TARGET_SECTION_DATE_R_3_10: process.env.TARGET_SECTION_DATE_R_3_10,
+      TARGET_SECTION_DATE_S_3_10: process.env.TARGET_SECTION_DATE_S_3_10,
+      TARGET_SECTION_DATE_A_3_10: process.env.TARGET_SECTION_DATE_A_3_10,
+
+      TARGET_AREA_R: process.env.TARGET_AREA_R,
+      TARGET_AREA_S: process.env.TARGET_AREA_S,
+      TARGET_AREA_A: process.env.TARGET_AREA_A,
+    });
+  }
+
+  /** 접속 주소를 담은 EnvConfig 인스턴스 만들기 */
+  private static parseUrlEnv() {
+    EnvConfig.loadEnv();
+
+    return this.createConfig({
+      MELON_TICKET: process.env.MELON_TICKET,
+    });
+  }
+
+  /** 카카오 계정 정보를 담은 EnvConfig 인스턴스 만들기 */
+  private static parseKakaoLoginEnv() {
+    EnvConfig.loadEnv();
+
+    return this.createConfig({
       ID: process.env.ID,
       PW: process.env.PW,
+    });
+  }
+  /** 필수 경로 변수를 담은 EnvConfig 인스턴스 만들기 */
+  private static parsePathEnv() {
+    EnvConfig.loadEnv();
+
+    return this.createConfig({
       TOP_LOGIN_BUTTON: process.env.TOP_LOGIN_BUTTON,
       KAKAO_LOGIN_BUTTON: process.env.KAKAO_LOGIN_BUTTON,
+      KAKAO_LOGIN_ID_INPUT: process.env.KAKAO_LOGIN_ID_INPUT,
+      KAKAO_LOGIN_PW_INPUT: process.env.KAKAO_LOGIN_PW_INPUT,
+      KAKAO_LOGIN_SUBMIT_BUTTON: process.env.KAKAO_LOGIN_SUBMIT_BUTTON,
     });
   }
 }
