7039a01735e69179641bc8ad99602ea9fff93f67
okonomiyakki <parkji12532@naver.com>
2024-02-13 11:18:05 +0900

    rename: DelayHandler 상위폴더 변경

--- src/handlers/DelayHandler.ts
+++ src/handlers/DelayHandler.ts
@@ -3,7 +3,7 @@ export default class DelayHandler {
    * 딜레이 만들어줌
    *
    * @param ms 미리초
-   * @returns
+   * @returns 딜레이
    */
   async delay(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
