2306d142b1bddbf6f834bc410a9fd9af3b649662
okonomiyakki <parkji12532@naver.com>
2024-02-13 11:18:21 +0900

    rename: ErrorHandler 상위폴더 변경

--- src/handlers/ErrorHandler.ts
+++ src/handlers/ErrorHandler.ts
@@ -6,10 +6,22 @@ export default class AppError extends Error {
     this.message = message;
   }
 
+  /**
+   * 페이지 접근 실패
+   *
+   * @param message 에러 내용
+   * @returns 에러 객체
+   */
   static pageNotFound(message: string) {
     return new AppError(message);
   }
 
+  /**
+   * 탐색 희망 인자값 유효성 검사 실패
+   *
+   * @param message 에러 내용
+   * @returns 에러 객체
+   */
   static badRequest(message: string) {
     return new AppError(message);
   }
