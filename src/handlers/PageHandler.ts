5c5d5c80b39c1bf933d91fa08d91577a79979fd9
okonomiyakki <parkji12532@naver.com>
2024-02-15 22:13:42 +0900

    comment: 주석 설명 달기

--- src/handlers/PageHandler.ts
+++ src/handlers/PageHandler.ts
@@ -3,15 +3,17 @@ import cheerio from 'cheerio';
 
 export default class PageHandler {
   /**
+   * 현재 페이지의 주소를 반환해줌
    *
    * @param page 현재 페이지
-   * @returns 현재 페이지 주소 반환
+   * @returns 현재 페이지 주소
    */
   getCurrentUrl(page: Page) {
     return page.url();
   }
 
   /**
+   * 현재 페이지 또는 프레임에 존재하는 특정 요소를 클릭해줌
    *
    * @param page 현재 페이지 or 프레임
    * @param path 해당 요소 selector 경로
@@ -23,6 +25,7 @@ export default class PageHandler {
   }
 
   /**
+   * 걍 카카오 로그인 자동화 메서드
    *
    * @param page 현재 페이지
    * @param idInputPath id 입력 input selector 경로
@@ -45,9 +48,10 @@ export default class PageHandler {
   }
 
   /**
+   * 현재 프레임의 전체 html 컨텐츠 내용을 반환해줌
    *
    * @param frame 현재 프레임
-   * @returns 현재 프레임의 전체 html 컨텐츠 내용 반환
+   * @returns html 컨텐츠 객체
    */
   async loadContent(frame: Frame) {
     const content = await frame.content();
